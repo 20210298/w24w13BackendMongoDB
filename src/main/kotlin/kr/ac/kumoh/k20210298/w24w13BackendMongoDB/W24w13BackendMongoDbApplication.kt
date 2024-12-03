e45d2f365af6227e740de92b544044e82b9ba22f
20210298 <144835269+20210298@users.noreply.github.com>
2024-12-03 17:33:09 +0900

    🎉 시작

---
+++ src/main/kotlin/kr/ac/kumoh/k20210298/w24w13BackendMongoDB/W24w13BackendMongoDbApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.k20210298.w24w13BackendMongoDB
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24w13BackendMongoDbApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24w13BackendMongoDbApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/k20210298/w24w13BackendMongoDB/W24w13BackendMongoDbApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.k20210298.w24w13BackendMongoDB
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w13BackendMongoDbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
